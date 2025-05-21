54bf745bd3863da83d25c0068c1d0c7b5849cc9f
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-21 23:35:04 +0530

    mongodb

---
+++ redis.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#check the user has root previlaiges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+# tee command will sent output to the multiple places, means to the log file and to the screen
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling default redis"
+
+dnf module enable redis:7 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling redis:7"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing redis"
+
+#stream editor, -i means permenant change, s for substitute, g for changes in complete file, /etc/mongod.conf is file location
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling redis"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB"
